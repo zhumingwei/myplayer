d2953b86d60532658f2c2499e5ece3ca82c40145
zhumingwei <zhumingwei2016@gmail.com>
2021-03-05 22:19:53 +0800

    update position

--- render.cpp
+++ render.cpp
@@ -113,7 +113,7 @@ int Init()
         0.5f, 0.5f, 0.0f, 1.0f, 0.0f,   // 右上角
         0.5f, -0.5f, 0.0f, 1.0f, 1.0f,  // 右下角
         -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // 左下角
-        -0.5f, 0.5f, 0.0f, 0.0f, 1.0f   // 左上角
+        -0.5f, 0.5f, 0.0f, 0.0f, 0.0f   // 左上角
     };
 
     unsigned int indices[] = {
@@ -204,7 +204,7 @@ void RenderFrame(AVFrame *frame)
     processInput(window);
     // render
     // ------
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     //draw triangle
